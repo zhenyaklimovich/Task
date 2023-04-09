62c33f124b23dba73645cbc0f3463fc6cb1bcc2c
«zhenya <zhenya-kunevich@mail.ru>
2023-04-09 16:57:57 +0300

    end

---
+++ Task07/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+    number = -number;
+if (number >= 100 && number <= 999)
+    Console.WriteLine(number % 10);
+else
+    Console.WriteLine("Вы ввели не трехзначчное число");
