9fcdc4ca4f0b062c3d8772b8cb646f169753e014
«zhenya <zhenya-kunevich@mail.ru>
2023-04-13 23:18:56 +0300

    prog

---
+++ Task14/Program.cs
@@ -0,0 +1,15 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool result = MTD (number);
+Console.WriteLine(result ? "да" : "нет");
+bool MTD (int num)
+{
+   return num %7==0 && num %23==0;
+}
