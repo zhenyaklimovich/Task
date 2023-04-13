59330f879b5691ce32fd3f564de5eb35f578da99
«zhenya <zhenya-kunevich@mail.ru>
2023-04-13 20:28:38 +0300

    Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if(firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+}
+
+else
+{
+    Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+}
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
