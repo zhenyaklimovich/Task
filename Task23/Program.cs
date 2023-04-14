7f8673401171cf7a3ff3093929de7d04ef924f8e
«zhenya <zhenya-kunevich@mail.ru>
2023-04-14 22:50:49 +0300

    prog

---
+++ Task23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+void TableAllSquares(int num)
+{
+int index = 1;
+while (index <= num)
+{
+Console.WriteLine($"{index, 3} {index * index * index, 4}");
+index++;
+}
+}
+
+Console.WriteLine("Please enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableAllSquares(number);
