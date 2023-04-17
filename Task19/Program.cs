3fb453114056c41144ed028f987d2f20aee2c8b9
«zhenya <zhenya-kunevich@mail.ru>
2023-04-17 23:11:58 +0300

    df

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -9,10 +9,10 @@ string? number = Console.ReadLine();
 
 void CheckingNumber(string number)
 {
-  if (number[0]==number[4] || number[1]==number[3]){
+  if (number[0]==number[4] && number[1]==number[3]){
     Console.WriteLine($"Ваше число: {number} - палиндром.");
   }
-  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+  else Console.WriteLine($"Ваше число: {number} - не палиндром.");
 }
 
 if (number!.Length == 5)
