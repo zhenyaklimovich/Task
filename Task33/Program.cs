e11b57fc2f33544af43bc1d21387a0fcdfaf6245
«zhenya <zhenya-kunevich@mail.ru>
2023-04-22 10:29:35 +0300

    Task33

---
+++ Task33/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+int[] arr = new int [size];
+Random rnd = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(min, max + 1);
+}
+return arr;
+}
+
+void PrintArray(int[] arr)
+{
+for (int i = 0; i < arr.Length; i++)
+{
+if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+else Console.Write($"{arr[i]}");
+}
+}
+
+bool CheckNumber(int num, int[] arr)
+{
+for (int i = 0; i < arr.Length; i++)
+{
+if(arr[i] == num) return true;
+}
+return false;
+}
+
+Console.WriteLine("Please enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArrayRndInt(5, -9, 9);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+
+bool checkNumber = CheckNumber(number, array);
+Console.WriteLine(checkNumber ? " Your numbers exists in array" : " Your numbers doesn't exits in the array");
