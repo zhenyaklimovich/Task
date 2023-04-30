0df9290bb19658fee43fb9ae19d6e42fee0dcbc9
«zhenya <zhenya-kunevich@mail.ru>
2023-04-30 20:57:36 +0300

    end

---
+++ Task51/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+// 0 1
+int[,] matrix = new int[rows, columns]; // 3, 4
+Random rnd = new Random();
+
+for (int i = 0; i < matrix.GetLength(0); i++) //matrix.GetLength(0) = 3
+{
+for (int j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(1) = 4
+{
+matrix[i, j] = rnd.Next(min, max + 1);
+}
+}
+return matrix;
+}
+
+int EvenElements(int[,] matrix)
+{
+int sum = 0;
+for (int i = 0; i < matrix.GetLength(0); i ++)
+{
+for (int j = 0; j < matrix.GetLength(1); j ++)
+{
+if (i == j)
+sum += matrix[i,j];
+}
+}
+return sum;
+
+}
+
+void PrintMatrix (int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+Console.Write("|");
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+Console.Write($"{matrix[i, j], 5} ");
+}
+Console.WriteLine(" |");
+}
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2d);
+EvenElements(array2d);
+int evenElements = EvenElements(array2d);
+Console.WriteLine($"Cумма элементов, находящихся на главной диагонали {evenElements}");
