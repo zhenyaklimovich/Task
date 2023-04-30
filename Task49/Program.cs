a6696949d2fe876187b3a081213db2d2921d2e83
«zhenya <zhenya-kunevich@mail.ru>
2023-04-30 20:34:20 +0300

    end

---
+++ Task49/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+
+int[,] matrix = new int[rows, columns];
+Random rnd = new Random();
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = rnd.Next(min, max + 1);
+}
+}
+return matrix;
+}
+
+void EvenElements(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i += 2)
+{
+for (int j = 0; j < matrix.GetLength(1); j += 2)
+{
+matrix[i, j] *= matrix[i, j];
+}
+}
+
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+Console.Write("|");
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+Console.Write($"{matrix[i, j],5} ");
+}
+Console.WriteLine(" |");
+}
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(array2d);
+EvenElements(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
