31fedef49b150fd9dce8f24e877b40e7fde28e8c
«zhenya <zhenya-kunevich@mail.ru>
2023-05-01 14:50:26 +0300

    end

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -6,38 +6,25 @@
 // 8 4 2 4
 // 1, 7 -> такого элемента в массиве нет
 
+Console.Write("Введите номер строки элемента матрицы: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца элемента матрицы: ");
+int columns = Convert.ToInt32(Console.ReadLine());
 
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
-// 0 1
-int[,] matrix = new int[rows, columns]; // 3, 4
+int[,] matrix = new int[rows, columns]; 
 Random rnd = new Random();
 
-for (int i = 0; i < matrix.GetLength(0); i++) //matrix.GetLength(0) = 3
+for (int i = 0; i < matrix.GetLength(0); i++) 
 {
-for (int j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(1) = 4
+for (int j = 0; j < matrix.GetLength(1); j++) 
 {
 matrix[i, j] = rnd.Next(min, max + 1);
 }
 }
 return matrix;
 }
-
-void PrintMatrix (int[,] matrix)
-{
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-Console.Write("|");
-for (int j = 0; j < matrix.GetLength(1); j++)
-{
-Console.Write($"{matrix[i, j], 5} ");
-}
-Console.WriteLine(" |");
-}
-}
-
-int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
-PrintMatrix(array2d);
 
 void SearchMatrix(int[,] matrix, int rows, int columns) // Поиск элемента в матрице.
 {
@@ -60,9 +47,19 @@ void SearchMatrix(int[,] matrix, int rows, int columns) // Поиск элеме
   }
 }
 
-Console.Write("Введите номер строки элемента матрицы: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите номер столбца элемента матрицы: ");
-int columns = Convert.ToInt32(Console.ReadLine());
+void PrintMatrix (int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+Console.Write("|");
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+Console.Write($"{matrix[i, j], 5} ");
+}
+Console.WriteLine(" |");
+}
+}
 
+int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2d);
 SearchMatrix(array2d, rows, columns);
