3f57a54ea019b00e83524b63495bb878f8ef8455
«zhenya <zhenya-kunevich@mail.ru>
2023-05-23 00:01:04 +0300

    end

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -1,16 +1,59 @@
-﻿int m = InputNumbers("Введите m: ");
-int n = InputNumbers("Введите n: ");
-int range = InputNumbers("Введите диапазон: от 1 до ");
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
 
-int[,] array = new int[m, n];
-CreateArray(array);
-WriteArray(array);
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+int[,] matrix = new int[rows, columns]; 
+Random rnd = new Random();
 
-Console.WriteLine($"\nОтсортированный массив: ");
-OrderArrayLines(array);
-WriteArray(array);
+for (int i = 0; i < matrix.GetLength(0); i++) 
+{
+for (int j = 0; j < matrix.GetLength(1); j++) 
+{
+matrix[i, j] = rnd.Next(min, max + 1);
+}
+}
+return matrix;
+}
 
-void OrderArrayLines(int[,] array)
+void PrintMatrix (int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+Console.Write("|");
+Console.Write($"{matrix[i, j], 5} ");
+}
+Console.WriteLine(" |");
+}
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 100);
+PrintMatrix(array2d);
+
+void ShowArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+        for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write("|");
+      Console.Write($"{arr[i, j], 5} ");
+    }
+      Console.WriteLine(" |");
+  }
+}
+
+void DescendingArrayNumbers(int[,] array)
 {
   for (int i = 0; i < array.GetLength(0); i++)
   {
@@ -29,32 +72,6 @@ void OrderArrayLines(int[,] array)
   }
 }
 
-int InputNumbers(string input)
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}
-
-void CreateArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      array[i, j] = new Random().Next(range);
-    }
-  }
-}
-
-void WriteArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      Console.Write(array[i, j] + " ");
-    }
-    Console.WriteLine();
-  }
-}
+Console.WriteLine($"Отсортированный массив: ");
+DescendingArrayNumbers(array2d);
+ShowArray(array2d);
