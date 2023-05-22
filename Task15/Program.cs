4043b96874a5c06f0e119b4836fa6b711f7552c8
«zhenya <zhenya-kunevich@mail.ru>
2023-05-22 22:22:08 +0300

    01

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -6,8 +6,10 @@
 Console.Write("Введи цифру, обозначающую день недели: ");
 int dayNumber = Convert.ToInt32(Console.ReadLine());
 
-void CheckingTheDayOfTheWeek (int dayNumber) {
-  if (dayNumber == 6 || dayNumber == 7) {
+void CheckingTheDayOfTheWeek (int dayNumber)
+{
+  if (dayNumber == 6 || dayNumber == 7)
+  {
   Console.WriteLine("(этот день выходной) -> да");
   }
   else if (dayNumber < 1 || dayNumber > 7) {
