5052689e35bda9626e08ccda1a241ee1182e2a8e
«zhenya <zhenya-kunevich@mail.ru>
2023-05-24 19:20:07 +0300

    end

---
+++ Task67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Напишите программу,
+// которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите целое натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigits = SumDigits(number);
+Console.WriteLine(sumDigits);
+
+int SumDigits(int num)//453//45//4
+{
+return num == 0 ? 0 : num % 10 + SumDigits(num / 10);
+}
